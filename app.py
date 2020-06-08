f04cb1a2aa26b449091b79f26af2c708fe6bf769
MackRoe <elaine.music@students.makeschool.com>
2020-06-08 10:37:10 -0500

    added MongoDB

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 
 ''' #TODO/DOING: set up an endpoint to deliver collected
 weather/mood data'''
 
+
 @app.route('/weather_mood_list')
-def weather_mood_list()
+def weather_mood_list():
     return
--- main.py
+++ main.py
@@ -12,18 +12,20 @@ obs = mgr.weather_at_place('San Francisco, US')
 weather = obs.weather
 f_temp = int(weather.temperature('fahrenheit')['temp'])
 
-print(weather)
-print('...')
-print(f'Your local weather is {weather.detailed_status} with a temperature of')
-print(f'{f_temp}{degree_sign}F')
-mood = input('How does that make you feel? ')
-print(f'When the weather is {weather.detailed_status} and {f_temp}{degree_sign}F, you feel {mood}.')
+def get_weathermood():
+    print(weather)
+    print('...')
+    print(f'Your local weather is {weather.detailed_status} with a temperature of')
+    print(f'{f_temp}{degree_sign}F')
+    mood = input('How does that make you feel? ')
+    print(f'When the weather is {weather.detailed_status} and {f_temp}{degree_sign}F, you feel {mood}.')
 
-current_date = [datetime.date(datetime.now()).month, datetime.date(datetime.now()).day, datetime.date(datetime.now()).year ]
-current_weather = [weather.detailed_status, f_temp]
-weather_mood_date = [current_weather, mood, current_date]
-print('...')
-print(list(weather_mood_date))
+    current_date = [datetime.date(datetime.now()).month, datetime.date(datetime.now()).day, datetime.date(datetime.now()).year ]
+    current_weather = [weather.detailed_status, f_temp]
+    weather_mood_date = [current_weather, mood, current_date]
+    print('...')
+    print(list(weather_mood_date))
+    return weather_mood_date
 
 # use pandas to create dataframe
 df = pd.DataFrame(weather_mood_date)
