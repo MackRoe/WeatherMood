984c766e375272f95494e35bf362d4c7080bbcca
MackRoe <elaine.music@students.makeschool.com>
2020-06-08 09:25:00 -0500

    add app.py for flask route/endpoint for collected data

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+''' #TODO/DOING: set up an endpoint to deliver collected
+weather/mood data'''
+
+@app.route('/weather_mood_list')
+def weather_mood_list()
+    return
