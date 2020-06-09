a81813fa765195b761733879c53c9f8e2bda6fe9
MackRoe <elaine.music@students.makeschool.com>
2020-06-09 08:41:11 -0500

    wm route works

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from datetime import datetime
 from main import weather_today, parse_date
+import os
+
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=f'{host}?retryWrites=false')
+
+db = client.WeatherMood
+weather = db.weather
+moods = db.moods
 
 app = Flask(__name__)
-
-client = MongoClient()
-db = client.WeatherMood
-weathermoods = db.weathermoods
 
 ''' #TODO/DOING: set up an endpoint to deliver collected
 weather/mood data'''
@@ -18,17 +24,25 @@ def index():
     return render_template('home.html', msg='Weather/Mood is Cool!!')
 
 
-@app.route('/weather_moods')
+@app.route('/wm')
 # route for list of each day's weather and mood
 # change '/weathermoods' to '/' after populating db
 def weathermood_index():
-    return render_template('weathermood_index.html', weathermoods=weathermoods)
+    return render_template('weathermood_index.html', moods=moods.find())
 
 
 @app.route('/new_mood')
 def new_mood():
     """Add a new mood entry"""
-    return render_template('new_mood.html')
+    mood = {
+        'mood': request.form.get('mood'),
+        'weather': request.form.get('weather'),
+        'created_at': datetime.now()
+    }
+    print(mood)
+    mood_id = moods.insert_one(mood).inserted_id
+    # print(request.form.to_dict())
+    return redirect(url_for('/', mood_id=mood_id))
 
 @app.route('/weather_today')
 def view_weather_today():
